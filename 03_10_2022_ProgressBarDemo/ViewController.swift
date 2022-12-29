fc598c7d0e1463218712e6e99e265527718afb89
aishwarya <ashp17star@gmail.com>
2022-12-29 12:41:53 +0530

    Initial Commit

---
+++ 03_10_2022_ProgressBarDemo/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  03_10_2022_ProgressBarDemo
+//
+//  Created by Vishal Jagtap on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var emailIdTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginProgressView: UIProgressView!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    let progress = Progress(totalUnitCount: 4)
+    var ratio : Float?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        loginProgressView.isHidden = true
+        ratio = 0
+        loginProgressView.progress = ratio!
+        
+        loginProgressView.layer.cornerRadius = 6
+        
+        loginButton.layer.cornerRadius = 7
+        
+    }
+
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        var userName = userNameTextField.text
+        var emailId = emailIdTextField.text
+        var password = passwordTextField.text
+        
+        if(userName != "" && emailId != "" && password != ""){
+            loginButton.isHidden = true
+            loginProgressView.isHidden = false
+            
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+                guard self.progress.isFinished == false else{
+                    timer.invalidate()
+                    return
+                }
+                
+                self.progress.completedUnitCount += 1
+                let progressFloat = Float(self.progress.fractionCompleted)
+                self.loginProgressView.setProgress(progressFloat, animated: true)
+            }
+        }
+    }
+}
